b0b5d882b3253fb4549493d60537348607a42042
Evgeny <evgenypoboykin@gmail.com>
2024-04-03 12:49:48 +0700

    remove unused vars

--- services/main.go
+++ services/main.go
@@ -1,14 +1,10 @@
 package services
 
 import (
-	"time"
-
 	"github.com/jackc/pgx/v5"
 )
 
 var db *pgx.Conn
-
-const dbTimeout = time.Second * 3
 
 func New(dbConnection *pgx.Conn) {
 	db = dbConnection
